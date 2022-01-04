9084cde0061801813a173160366f6c0ab110af25
kmfrick <kmfrick98@gmail.com>
2022-01-04 12:07:53 +0100

    Add distance check

--- Assets/MusicianPlayScript.cs
+++ Assets/MusicianPlayScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MusicianPlayScript : MonoBehaviour
 {
+    public readonly float DIST_THRESHOLD = 5.0f;
+    public GameObject Instrument;
 
     // Start is called before the first frame update
     void Start()
@@ -13,6 +15,15 @@ public class MusicianPlayScript : MonoBehaviour
 
     void OnPointerClick()
     {
+        var dist = Vector3.Distance(transform.position, Instrument.transform.position);
+        if (dist < DIST_THRESHOLD)
+        {
+            Debug.Log("Instrument close enough. Will play song.");
+        }
+        else
+        {
+            Debug.Log("Instrument too far. Cannot play.");
+        }
         Debug.Log("CLICKED ON MUSICIAN.");
     }
 
